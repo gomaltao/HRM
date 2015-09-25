3014d5f93096d6c34c8b9d14b9662031dc11fdf2
gabriel tekin <gabriel.tekin@outlook.com>
2015-09-25 11:27:56 +0200

    First try of Timecard syststem

---
+++ DBLayer/EmpManContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using DBLayer.Model;
+
+namespace DBLayer
+{
+    public class EmpManContext : DbContext
+    {
+        public DbSet<Employee> {get; set;}
+    }
+}
